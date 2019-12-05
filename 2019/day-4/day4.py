922803a6ed904918fc469a1c90f679a2502b2a0d
Yulo Leake <yuloleake@gmail.com>
2019-12-04 21:16:33 -0500

    clean up

--- 2019/day-4/day4.py
+++ 2019/day-4/day4.py
@@ -1,18 +1,21 @@
-""" Day 4 of advent of code
+""" Day 4: Secure Container
 
 Valid password criteria:
   1. It is a six-digit number.
   2. Two adjacent digits are the same (like 22 in 122345).
   3. The value is within the range given in your puzzle input.
-  4. Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-  5. Two adjacent matching digits are not part of a larger group of matching digits. (only for part 2)
+  4. Going from left to right, the digits never decrease; they only ever 
+      increase or stay the same (like 111123 or 135679).
+  5. Two adjacent matching digits are not part of a larger group of matching 
+      digits. (only for part 2)
 """
 from itertools import groupby
 
 def is_ascending(password: str) -> bool:
     return ''.join(sorted(password)) == password
 
-def is_valid_pw(password: str, part_2=False) -> bool:
+
+def is_valid_pw(password: str) -> bool:
     """ Whether given password is valid or not.
 
     Covers criteria 1 and 2.
@@ -26,13 +29,14 @@ def is_valid_pw(password: str, part_2=False) -> bool:
     adjacents = [adj for adj in adjacents if len(adj) > 1]
     has_good_adjacent = len(adjacents) > 0
 
-    if has_good_adjacent and part_2:
-        # criteria 5
-        adjacents = [adj for adj in adjacents if len(adj) < 3]
-        has_good_adjacent = len(adjacents) > 0
-
     return is_six_digit and has_good_adjacent
 
+
+def meets_part_2(password: str) -> bool:
+    """ Check whether given password meets Criteria 5 (part 2). """
+    adjacents = [''.join(group) for _, group in groupby(password)]
+    adjacents = [adj for adj in adjacents if len(adj) == 2]
+    return len(adjacents) > 0
 
 
 if __name__ == '__main__':
@@ -55,6 +59,7 @@ if __name__ == '__main__':
     for i in range(start, end+1): # Criteria 3
         pw = str(i)
         if is_ascending(pw): # Criteria 4
+            # Criteria 1 and 2
             pws += 1 if is_valid_pw(pw) else 0
     print(f'part 1 = {pws}')
 
@@ -67,14 +72,16 @@ if __name__ == '__main__':
     }
 
     for pw in pws.keys():
-        assert (is_ascending(pw) and is_valid_pw(pw, True)) == pws[pw]
+        is_valid = is_ascending(pw) and is_valid_pw(pw) and meets_part_2(pw)
+        assert is_valid == pws[pw]
 
     start = 158126
     end   = 624574
-    
+
     pws = 0
     for i in range(start, end+1): # Criteria 3
         pw = str(i)
         if is_ascending(pw): # Criteria 4
-            pws += 1 if is_valid_pw(pw, True) else 0
+            # Criteria 1, 2, and 5
+            pws += 1 if is_valid_pw(pw) and meets_part_2(pw) else 0
     print(f'part 2 = {pws}')
